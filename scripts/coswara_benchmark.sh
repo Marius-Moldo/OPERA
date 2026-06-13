a35d51752f7d12371a3d5a07ec88213a7f216b98
Marius Moldovan <68606110+Marius-Moldo@users.noreply.github.com>
2026-06-13 14:18:33 +0200

    Revert "refactor: rename CoughPhase-CLR to CoughSound-CLR in benchmark scripts"
    This reverts commit f2ca9b6ccc726a577e3bb282bc27ae58ec222f1d.

--- scripts/coswara_benchmark.sh
+++ scripts/coswara_benchmark.sh
@@ -2,11 +2,11 @@
 source "$(dirname "$0")/lib/benchmark_common.sh"
 
 PRETRAINS=(
-  "cks/model/combined/coughvid_covidUKcough/CoughSound-CLR-20pct-epoch=319--valid_acc=0.27-valid_loss=3.2871.ckpt"
-  "cks/model/combined/coughvid_covidUKcough/CoughSound-CLR-40pct-epoch=159--valid_acc=0.35-valid_loss=2.9020.ckpt"
-  "cks/model/combined/coughvid_covidUKcough/CoughSound-CLR-60pct-epoch=99--valid_acc=0.35-valid_loss=2.8306.ckpt"
-  "cks/model/combined/coughvid_covidUKcough/CoughSound-CLR-80pct-epoch=79--valid_acc=0.38-valid_loss=2.6780.ckpt"
-  "cks/model/combined/coughvid_covidUKcough/CoughSound-CLR-epoch=59--valid_acc=0.36-valid_loss=2.8016.ckpt"
+  "cks/model/combined/coughvid_covidUKcough/CoughPhase-CLR-20pct-epoch=319--valid_acc=0.27-valid_loss=3.2871.ckpt"
+  "cks/model/combined/coughvid_covidUKcough/CoughPhase-CLR-40pct-epoch=159--valid_acc=0.35-valid_loss=2.9020.ckpt"
+  "cks/model/combined/coughvid_covidUKcough/CoughPhase-CLR-60pct-epoch=99--valid_acc=0.35-valid_loss=2.8306.ckpt"
+  "cks/model/combined/coughvid_covidUKcough/CoughPhase-CLR-80pct-epoch=79--valid_acc=0.38-valid_loss=2.6780.ckpt"
+  "cks/model/combined/coughvid_covidUKcough/CoughPhase-CLR-epoch=59--valid_acc=0.36-valid_loss=2.8016.ckpt"
   "cks/model/combined/coughvid_covidUKcough/OPERA-CE-Cough-20pct-epoch=849--valid_acc=0.04-valid_loss=4.6404.ckpt"
   "cks/model/combined/coughvid_covidUKcough/OPERA-CE-Cough-40pct-epoch=549--valid_acc=0.62-valid_loss=1.8038.ckpt"
   "cks/model/combined/coughvid_covidUKcough/OPERA-CE-Cough-60pct-epoch=449--valid_acc=0.69-valid_loss=1.5890.ckpt"
@@ -15,11 +15,11 @@ PRETRAINS=(
 )
 
 OUTPUT_NAMES=(
-  "CoughSound-CLR-20pct"
-  "CoughSound-CLR-40pct"
-  "CoughSound-CLR-60pct"
-  "CoughSound-CLR-80pct"
-  "CoughSound-CLR"
+  "CoughPhase-CLR-20pct"
+  "CoughPhase-CLR-40pct"
+  "CoughPhase-CLR-60pct"
+  "CoughPhase-CLR-80pct"
+  "CoughPhase-CLR"
   "OPERA-CE-Cough-20pct"
   "OPERA-CE-Cough-40pct"
   "OPERA-CE-Cough-60pct"
